aa8106f38a139b3fd02f3c04a99227432dbe280c
Sarkis T <sarkis308@gmail.com>
2020-01-21 17:27:30 -0500

    Project Setup

---
+++ Maze/Source/Maze/MazeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MazeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMazeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMazeGameMode();
+};
+
+
+
