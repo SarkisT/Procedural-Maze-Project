073f6ffe930d5c68c1aa1959e5c21454fac0f433
Sarkis T <sarkis308@gmail.com>
2020-01-27 00:58:13 -0500

    +Working Terrain! (MazeBP)
    +Can now spawn tiles and can also choose the sizes of the tiles.
    +Currently we have only 1 tile, BasicFloorBP.

---
+++ Maze/Source/Maze/Terrain.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BasicFloor.h"
+#include "Terrain.generated.h"
+
+
+UCLASS()
+class MAZE_API ATerrain : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATerrain();
+
+	//UPROPERTY(VisibleAnywhere)
+	//UStaticMeshComponent* BaseFloor;
+
+	//Mesh Settings
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int NumberMeshesX;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int NumberMeshesY;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MeshWidth = 100.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MeshLength = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = Floor)
+	TSubclassOf<AActor> CurrentFloor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector OriginalLocation;
+
+	UFUNCTION(BluePrintCallable)
+	void Spawn(FVector SpawnLocation, FRotator SpawnRotation);
+
+
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
